52aeafd1dd14f0f4ba5dcedf6d7d095d8451b2fd
yuyansong <1259581033@qq.com>
2025-08-11 08:20:41 +0800

    初始化项目Eino

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/cloudwego/eino-ext/components/model/ark"
+	"github.com/cloudwego/eino/schema"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	// fmt.Print(viper.GetString("ARK_API_KEY"))
+	ctx := context.Background()
+
+	timeout := 30 * time.Second
+	// 初始化模型
+	model, err := ark.NewChatModel(ctx, &ark.ChatModelConfig{
+		APIKey:  viper.GetString("ARK_API_KEY"),
+		Model:   viper.GetString("ARK_MODEL"),
+		Timeout: &timeout,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// 准备消息
+	messages := []*schema.Message{
+		schema.SystemMessage("你是一个助手"),
+		schema.UserMessage("你好"),
+	}
+
+	// 生成回复
+	response, err := model.Generate(ctx, messages)
+	if err != nil {
+		panic(err)
+	}
+
+	// 处理回复
+	println(response.Content)
+
+	// 获取 Token 使用情况
+	if usage := response.ResponseMeta.Usage; usage != nil {
+		println("提示 Tokens:", usage.PromptTokens)
+		println("生成 Tokens:", usage.CompletionTokens)
+		println("总 Tokens:", usage.TotalTokens)
+	}
+}
